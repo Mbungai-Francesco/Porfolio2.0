410de468b6aa1bc976254b3a69ae3b5fb769a0be
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2025-06-09 10:27:50 +0100

    contact responsive done

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,14 +3,14 @@ import { Linkedin, Mail } from 'lucide-react'
 
 const Contact = () => {
   return (
-    <div className={cn(`pb-14 px-30 h-[80vh] flex flex-col justify-between`)}>
+    <div className={cn(`pb-14 px-30 h-[80vh] flex flex-col justify-between items-center`)}>
       <div className={cn(`space-y-8`)}>
         <div className={cn(`w-fit mx-auto text-center flex flex-col `)}>
           <p className={cn(`text-black/70 text-base`)}>Get in Touch</p>
           <h2 className={cn(`text-5xl font-semibold`)}>Contact Me</h2>
         </div>
         <div
-          className={cn(`border flex w-fit mx-auto p-4 rounded-3xl space-x-4`)}
+          className={cn(`border flex flex-col sm:flex-row w-fit mx-auto p-4 rounded-3xl space-x-4 gap-4 sm:gap-0`)}
         >
           <div className={cn(`flex items-center space-x-2`)}>
             <Mail />
